7656cf00a73266f61e1d0ea574b85a0c3c587abb
aeberharhsr <19borki94@gmail.com>
2020-09-15 18:07:12 +0200

    initialize functions

--- plagiarism-checker.py
+++ plagiarism-checker.py
@@ -1,6 +1,38 @@
 import os
 import csv
-
+import sys
 
 # script will be divided into some main parts
-# at first functions to load the files in each supported format
+# sys args from the user input
+
+
+# function to load a pdf document 
+def load_pdf():
+    pass
+
+# function to load a csv document
+def load_csv():
+    pass
+
+# function to load a txt document
+def load_txt():
+    pass
+
+# function to load a docx document
+def load_docx():
+    pass
+
+# function to store every word in the document in an array in order as its occurrence
+def store_pdf_word():
+    pass
+
+def store_csv_word():
+    pass
+
+def store_txt_word():
+    pass
+
+def store_docx_word():
+    pass
+
+
