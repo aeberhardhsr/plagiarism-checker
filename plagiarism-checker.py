6377cd34fd8ad71a280012015e1b26fd8ad0150b
aeberharhsr <19borki94@gmail.com>
2020-09-15 21:44:51 +0200

    update functions

--- plagiarism-checker.py
+++ plagiarism-checker.py
@@ -1,44 +1,50 @@
 import os
 import csv
 import sys
+import numpy as np
 
 # script will be divided into some main parts
 # sys args from the user input
 
 
-# function to load a pdf document 
-def load_pdf():
-    pass
-
-# function to load a csv document
-def load_csv():
-    pass
-
 # function to load a txt document
-def load_txt():
-    pass
-
-# function to load a docx document
-def load_docx():
-    pass
-
-# function to store every word in the document in an array in order as its occurrence
-def store_pdf_word():
-    pass
+def load_origin_txt(origin_txt_file):
+    origin_word_list = []
+    with open(origin_txt_file) as file:
+        for line in file:
+            for word in line.split():
+                origin_word_list.append(word)
+    return origin_word_list
 
 # function to store every word in the document in an array in order as its occurrence
-def store_csv_word():
+def load_check_txt(check_txt_file):
+    check_word_list = []
+    with open(check_txt_file) as file:
+        for line in file:
+            for word in line.split():
+                check_word_list.append(word)
+    return check_word_list
+
+# function to join both lists and eliminate duplicates
+# each word from each document occurs exactly once
+# result is sorted ascending
+def list_combining(origin_list, check_list):
+    return sorted(np.unique(origin_list + check_list))
+    
+# function returns 0 or 1 if the word occurs in origin document
+def find_word_origin(combined_word_list, origin_word_list ):
     pass
 
-# function to store every word in the document in an array in order as its occurrence
-def store_txt_word():
+# function returns 0 or 1 if the word occurs in check document
+def find_word_check(combined_word_list, check_word_list):
     pass
-
-# function to store every word in the document in an array in order as its occurrence
-def store_docx_word():
-    pass
-
 
 # main part of the script
 if __name__ == "__main__":
-    load_txt()
+    origin_file = load_origin_txt('origin-file.txt')
+    check_file = load_check_txt('file-to-check.txt')
+    #print(check_word_list)
+    #print(list_combining(load_origin_txt('origin-file.txt'), load_check_txt('file-to-check.txt')))
+    combined_list = list_combining(load_origin_txt('origin-file.txt'), load_check_txt('file-to-check.txt'))
+    find_word_origin(combined_list, origin_file)
+    find_word_check(combined_list, check_file)
